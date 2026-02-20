9e1c745170254d9b626beac62bed5dab6771eed1
Manuel Ibar <manuel.ibar@gmail.com>
2026-02-20 16:54:01 -0300

    allow for stdin

--- cmd/shake/main.go
+++ cmd/shake/main.go
@@ -7,79 +7,104 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"strings"
 
 	"github.com/mibar/tree-shaker/pkg/shaker"
 )
 
-func ptr[T any](v T) *T { return &v }
+const usage = `usage: shake <include|exclude> [flags] <path> [path...]
+
+Input sources (first match wins):
+  -file <path>    Read from file
+  -input <json>   Read from argument
+  (default)       Read from stdin
+
+Examples:
+  shake include -input '{"a":1,"b":2}' '$.a'
+  shake include -file data.json -pretty '$.name' '$.email'
+  curl -s url | shake include '$.data[*].id'`
 
 func main() {
-	mode := flag.String("mode", "include", `"include" or "exclude"`)
-	paths := flag.String("paths", "", "comma-separated JSONPath expressions")
-	file := flag.String("file", "", "path to input JSON file (default: read from stdin)")
-	output := flag.String("output", "", "path to output JSON file (default: write to stdout)")
-	maxDepth := flag.Int("max-depth", 0, fmt.Sprintf("maximum JSON nesting depth (default: %d, -1 = no limit)", shaker.MaxDepth))
-	maxPathLength := flag.Int("max-path-length", 0, fmt.Sprintf("maximum byte length per JSONPath expression (default: %d, -1 = no limit)", shaker.MaxPathLength))
-	maxPathCount := flag.Int("max-path-count", 0, fmt.Sprintf("maximum number of JSONPath expressions (default: %d, -1 = no limit)", shaker.MaxPathCount))
-	pretty := flag.Bool("pretty", false, "pretty-print the JSON output")
-	flag.Parse()
-
-	if *paths == "" {
-		fmt.Fprintln(os.Stderr, "usage: shake -mode include -paths '$.name,$.email' [-file input.json | < input.json] [-output result.json]")
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, usage)
 		os.Exit(1)
 	}
 
-	var r io.Reader = os.Stdin
-	if *file != "" {
+	mode := os.Args[1]
+	if mode != "include" && mode != "exclude" {
+		fmt.Fprintf(os.Stderr, "invalid mode: %q (expected \"include\" or \"exclude\")\n\n%s\n", mode, usage)
+		os.Exit(1)
+	}
+
+	fs := flag.NewFlagSet("shake", flag.ExitOnError)
+	file := fs.String("file", "", "path to input JSON file")
+	input := fs.String("input", "", "inline JSON string")
+	output := fs.String("output", "", "path to output JSON file (default: stdout)")
+	maxDepth := fs.Int("max-depth", 0, fmt.Sprintf("maximum JSON nesting depth (default: %d, -1 = no limit)", shaker.MaxDepth))
+	maxPathLength := fs.Int("max-path-length", 0, fmt.Sprintf("maximum byte length per JSONPath expression (default: %d, -1 = no limit)", shaker.MaxPathLength))
+	maxPathCount := fs.Int("max-path-count", 0, fmt.Sprintf("maximum number of JSONPath expressions (default: %d, -1 = no limit)", shaker.MaxPathCount))
+	pretty := fs.Bool("pretty", false, "pretty-print the JSON output")
+	fs.Usage = func() { fmt.Fprintln(os.Stderr, usage) }
+	fs.Parse(os.Args[2:])
+
+	paths := fs.Args()
+	if len(paths) == 0 {
+		fmt.Fprintln(os.Stderr, "error: at least one JSONPath expression is required")
+		fmt.Fprintln(os.Stderr, usage)
+		os.Exit(1)
+	}
+
+	var data []byte
+	switch {
+	case *file != "":
 		f, err := os.Open(*file)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "open file: %v\n", err)
 			os.Exit(1)
 		}
 		defer f.Close()
-		r = f
+		data, err = io.ReadAll(f)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "read file: %v\n", err)
+			os.Exit(1)
+		}
+	case *input != "":
+		data = []byte(*input)
+	default:
+		var err error
+		data, err = io.ReadAll(os.Stdin)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "read stdin: %v\n", err)
+			os.Exit(1)
+		}
 	}
-
-	input, err := io.ReadAll(r)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "read input: %v\n", err)
-		os.Exit(1)
-	}
-
-	pathList := strings.Split(*paths, ",")
 
 	var q shaker.Query
-	switch *mode {
+	switch mode {
 	case "include":
-		q = shaker.Include(pathList...)
+		q = shaker.Include(paths...)
 	case "exclude":
-		q = shaker.Exclude(pathList...)
-	default:
-		fmt.Fprintf(os.Stderr, "invalid mode: %q (expected \"include\" or \"exclude\")\n", *mode)
-		os.Exit(1)
+		q = shaker.Exclude(paths...)
 	}
 
-	// Limits: 0 = use default, >0 = custom, <0 = no limit (Ptr(0)).
 	var limits shaker.Limits
 	if *maxDepth > 0 {
 		limits.MaxDepth = maxDepth
 	} else if *maxDepth < 0 {
-		limits.MaxDepth = ptr(0)
+		limits.MaxDepth = new(int)
 	}
 	if *maxPathLength > 0 {
 		limits.MaxPathLength = maxPathLength
 	} else if *maxPathLength < 0 {
-		limits.MaxPathLength = ptr(0)
+		limits.MaxPathLength = new(int)
 	}
 	if *maxPathCount > 0 {
 		limits.MaxPathCount = maxPathCount
 	} else if *maxPathCount < 0 {
-		limits.MaxPathCount = ptr(0)
+		limits.MaxPathCount = new(int)
 	}
 	q = q.WithLimits(limits)
 
-	out, err := shaker.Shake(input, q)
+	out, err := shaker.Shake(data, q)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "shake: %v\n", err)
 		os.Exit(1)
